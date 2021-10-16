9520593938db7fd0832e12d31192405f37d42fa1
iksanova <limma@yandex.ru>
2021-10-16 11:18:08 +0300

    fix

--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -18,24 +18,3 @@ class TestAuthorizationClass:
         url = browser.current_url
         browser.get_cookie("session")
         assert url == "https://qastand.valhalla.pw/profile"
-
-    @pytest.mark.parametrize("email,password", NEGATIVE_LOGIN_CREDENTIALS, ids=["test1", "test2", "test3", "test4"])
-    def test_login_negatives(self, email, password):
-        with Chrome() as browser:
-            browser.maximize_window()
-            browser.get('https://qastand.valhalla.pw/login')
-            wait_until_clickable(browser, (By.NAME, 'email')).send_keys(email)
-            wait_until_clickable(browser, (By.NAME, 'password')).send_keys(password)
-            wait_until_clickable(browser, (By.CLASS_NAME, 'button')).click()
-            assert wait_until_present(browser, (By.CLASS_NAME, 'is-danger'))
-
-    # @pytest.mark.parametrize("email", PARAMETER_FOR_TEST_EMAIL)
-    # @pytest.mark.parametrize("password", PARAMETER_FOR_TEST_PASS)
-    # def test_login_negatives(self, email, password):
-    #     with Chrome() as browser:
-    #         browser.maximize_window()
-    #         browser.get('https://qastand.valhalla.pw/login')
-    #         wait_until_clickable(browser, (By.NAME, 'email')).send_keys(email)
-    #         wait_until_clickable(browser, (By.NAME, 'password')).send_keys(password)
-    #         wait_until_clickable(browser, (By.CLASS_NAME, 'button')).click()
-    #         assert wait_until_not_present(browser, (By.CLASS_NAME, 'is-danger'))
