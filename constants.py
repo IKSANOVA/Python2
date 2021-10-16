e8378284f1773686256531961a0bc28c192f7a67
iksanova <limma@yandex.ru>
2021-10-16 11:21:25 +0300

    fix 4

--- constants.py
+++ constants.py
@@ -11,6 +11,7 @@ POSITIVE_LOGIN_CREDENTIALS = {"email": "qa_test@test.ru",
 page_login = "https://qastand.valhalla.pw/login"
 page_blog = "https://qastand.valhalla.pw/blog/author/2/"
 
+
 class Links:
     base_url = "https://qastand.valhalla.pw/"
     login = base_url + "login"
@@ -18,6 +19,7 @@ class Links:
     blog = base_url + "blog"
 
 
-SESSION_COOKIE = {'name': 'session',
-                  'value': '.eJwlzjsOwjAMANC7ZGawHX-SXqaKY0ewtnRC3J1KTG99n7KvI89n2d7HlY-yv6JsBaMOY1RaQNzqSp05QsSpazaXI'                  'FATqiOH1qnCbI3SQRHSAmG26k1o9NWhMknrWN37jU0IU45wme5GiyWBHLkj8mzc09y03JHrzOO_ofL9AVk6Le8.YVn0'
-                           'oA.WT_USo8F4bTkxwenEGx0DU0ZzwQ'}
+SESSION_COOKIE = {"name": 'session',
+                  "value": '.eJwlzjEOwjAMRuG7ZGbI79hN3MtUcmwL1pROiLtTif096fuUI1ecz7K_1xWPcry87MXJYsTcIIQ2h5EGTCAT7NA'
+                           '-muVmLl6pE3MN60jUqdInFDyamDQHRzZKakhKjNpUddyXdGUWdmEdd8zgTrOKwKhap6xcbsh1xvprpHx_OG4syg'
+                           '.YWqCMg.L--C-U8_k9BoHAWPTIkpCoa1M4I'}
