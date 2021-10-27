49b719fe118abaf3920bd04435dd1dee5944e643
iksanova <limma@yandex.ru>
2021-10-27 23:18:10 +0300

    commit config

--- tests/tests_blog/test_blog.py
+++ tests/tests_blog/test_blog.py
@@ -1,5 +1,6 @@
 import pytest
 from selenium.webdriver.common.by import By
+from webbrowser import UnixBrowser
 
 from constants import Links
 from functions import wait_until_clickable, wait_until_present, login_ui
@@ -8,8 +9,8 @@ from functions import wait_until_clickable, wait_until_present, login_ui
 @pytest.mark.smoke
 class TestBlogClass:
 
-    def test_blog_positive(self, browser):
-        browser.get(url + Links.blog)
+    def test_blog_positive(self, browser: UnixBrowser):
+        browser.open_new(url + Links.blog)
         wait_until_clickable(browser, (By.CSS_SELECTOR, "[href = '/blog/page/1/test-post/']")).click()
         element = wait_until_clickable(browser, (By.CSS_SELECTOR, "p:nth-child(5)")).text
         assert element == 'Hello world!'
