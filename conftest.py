abfb2cdaa3d81e88eccdb542bd181a21cc5a5121
iksanova <limma@yandex.ru>
2021-11-06 21:39:06 +0300

    remote

--- conftest.py
+++ conftest.py
@@ -3,16 +3,29 @@ import random
 import pytest
 
 from api.api_client import Client
-from constants import Links, VALID_BROWSERS
+from constants import VALID_BROWSERS, BROWSER_REMOTE_CAPABILITIES, COMMAND_EXECUTOR, Links
 
 
 @pytest.fixture()
 def browser(request):
     launch = request.config.getoption("--launch")
-    browser = VALID_BROWSERS[launch]()
+    if launch == 'remote':
+        browser = VALID_BROWSERS[launch](command_executor=COMMAND_EXECUTOR,
+                                         desired_capabilities=BROWSER_REMOTE_CAPABILITIES)
+    else:
+        browser = VALID_BROWSERS[launch]()
     browser.maximize_window()
     yield browser
     browser.quit()
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--env", default="prod"
+    )
+    parser.addoption(
+        "--launch", default="chrome", choices=["chrome", "opera", "remote"]
+    )
 
 
 @pytest.fixture(scope="session")
--- constants.py
+++ constants.py
@@ -1,4 +1,22 @@
-from selenium.webdriver import Opera, Chrome
+from selenium.webdriver import Opera, Chrome, Remote
+
+VALID_BROWSERS = {
+   "chrome": Chrome,
+   "opera": Opera,
+   "remote": Remote
+}
+
+BROWSER_REMOTE_CAPABILITIES = {
+      "browserName": "chrome",
+      "version": "95.0",
+      "enableVNC": True,
+  }
+
+SELENOID_URL = 'http://localhost:4444/wd/hub'
+
+
+COMMAND_EXECUTOR = 'http://localhost:4444/wd/hub'
+
 
 NEGATIVE_LOGIN_CREDENTIALS = [
     ("", "!QAZ2wsx"),
@@ -18,9 +36,3 @@ class Links:
     login = "login"
     profile = "profile"
     blog = "blog"
-
-
-VALID_BROWSERS = {
-   "chrome": Chrome,
-   "opera": Opera
-}
